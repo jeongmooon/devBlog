cfc07d841111a9c19683047c7a227e3f04ee4301
jeong moon <ijhghji@gmail.com>
2022-01-22 21:40:08 +0900

    수정

--- blog/src/components/main/MainComponent.jsx
+++ blog/src/components/main/MainComponent.jsx
@@ -31,9 +31,31 @@ const ItemWrap = styled.div`
 `;
 
 const BoardItem = styled.div`
-    position: relative;
+    font-family: 'KOTRA_SONGEULSSI';
     width: 175px;
-`
+    height: 175px;
+    margin: 0.5rem;
+    border-radius: 5px;
+    position: relative;
+    background-size: cover;
+    
+    ::before {
+    content: "";
+    opacity: 0.5;
+    border-radius: 5px;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    background-color: #000;
+    }
+
+    @media only screen and (min-width: 768px) {
+    width: 250px;
+    height: 250px;
+}
+`;
 
 const BoardTitle = styled.p`
     color : ${palette.main};
@@ -49,14 +71,32 @@ function MainComponent() {
       <MainBlock>
           <CategoryTitle>FrontEnd</CategoryTitle>
           <ItemWrap>
-              <BoardItem>
-                  <BoardTitle>HTML</BoardTitle>
-                  <BoardTitle>JAVASCRIPT</BoardTitle>
-                  <BoardTitle>REACT</BoardTitle>
-              </BoardItem>
+            <BoardItem src="" alt="">
+                <BoardTitle>HTML</BoardTitle>                  
+            </BoardItem>
+            <BoardItem>
+                <BoardTitle>JAVASCRIPT</BoardTitle>
+            </BoardItem>
+            <BoardItem>
+                <BoardTitle>REACT</BoardTitle>
+            </BoardItem>
           </ItemWrap>
           <CategoryTitle>BackEnd</CategoryTitle>
+              <BoardItem>
+                  <BoardTitle>JAVA</BoardTitle>
+                  <BoardTitle>Node.js</BoardTitle>
+                  <BoardTitle>REACT</BoardTitle>
+              </BoardItem>
+          <CategoryTitle>DB</CategoryTitle>
+              <BoardItem>
+                  <BoardTitle>SQL</BoardTitle>
+                  <BoardTitle>MongoDB</BoardTitle>
+              </BoardItem>
           <CategoryTitle>Cloud</CategoryTitle>
+              <BoardItem>
+                  <BoardTitle>AWS</BoardTitle>
+                  <BoardTitle>Firebase</BoardTitle>
+              </BoardItem>
       </MainBlock>
   )
 }
