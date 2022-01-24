188d81c569a90e7c4d29a340552b54ae17a5b0e5
jeong moon <ijhghji@gmail.com>
2022-01-24 23:57:58 +0900

    수정

---
+++ blog/src/components/study/HtmlComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HtmlBlock = styled.div`
+`
+
+function HtmlComponent() {
+  return (
+      <HtmlBlock>
+          
+      </HtmlBlock>
+  );
+}
+
+export default HtmlComponent;
---
+++ blog/src/containers/study/HtmlContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HtmlComponent from '../../components/study/HtmlComponent';
+
+function HtmlContainer() {
+  return (
+      <HtmlComponent />
+  )
+}
+
+export default HtmlContainer;
