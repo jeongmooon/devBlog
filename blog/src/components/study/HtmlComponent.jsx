7f2634b78581af05416b8c0a4dc8e33ead3f3865
jeong moon <ijhghji@gmail.com>
2022-02-12 23:45:31 +0900

    수정

--- blog/src/components/study/HtmlComponent.jsx
+++ blog/src/components/study/HtmlComponent.jsx
@@ -7,7 +7,9 @@ const HtmlBlock = styled.div`
 function HtmlComponent() {
   return (
       <HtmlBlock>
-          
+          <img src=""></img>
+          <h1>내용</h1>
+          <div>하드코딩</div>
       </HtmlBlock>
   );
 }
